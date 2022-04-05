90723779bd49e3bab8c383d2b1b3fd59946d09c8
belalbahaa1 <belalbahaa800@gmail.com>
2022-04-05 10:21:29 +0200

    solution

---
+++ script.js
@@ -0,0 +1,35 @@
+const cardContent = document.querySelector(".rating-card");
+const ratingContent = document.querySelector(".rating-state");
+const resultCard = document.querySelector(".result-card");
+const submitBtn = document.querySelector(".submit-btn");
+const ratingNum = document.querySelectorAll(".rating-number");
+const selection = document.querySelector(".selection");
+let score = 0;
+
+// selection.innerHTML = "2"
+
+// to clear previous choices cuz you only make one
+
+const clearSelection = () => {
+  ratingNum.forEach((rating) => {
+    rating.classList.remove("selected");
+  });
+};
+
+ratingContent.addEventListener("click", (e) => {
+  clearSelection();
+  if ((e.target.classList = "rating-number")) {
+    e.target.classList.add("selected");
+    score = e.target.innerHTML;
+    submitBtn.style.background = "hsl(0, 0%, 100%)";
+    submitBtn.style.color = "hsl(25, 97%, 53%)";
+    selection.innerHTML = score;
+  }
+});
+
+const showResult = () => {
+  cardContent.classList.add("card-visible");
+  resultCard.classList.remove("card-visible");
+};
+
+submitBtn.addEventListener("click", showResult);
